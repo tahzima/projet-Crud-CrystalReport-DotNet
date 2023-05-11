4d714ea4fa195d333588822ae23e9e00e4a65250
tahzima <tahzima.i@gmail.com>
2023-05-11 17:18:06 +0100

    projet c# Ado Net Crud + Crystal Report

---
+++ projetC#BD/Form1.cs
@@ -0,0 +1,162 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projetC_BD
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connexion = new MySqlConnection("Server=localhost;database=adodb;Uid=root;Pwd=;");
+        public Form1()
+        {
+            InitializeComponent();
+            actualiser();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            actualiser();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void vid()
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+        }
+        public void actualiser()
+        {
+            MySqlCommand command = new MySqlCommand("SELECT * FROM etudiant", connexion);
+            DataTable dataTable = new DataTable();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+
+        }
+
+        public void ajout(string nom, string prenom, string cne)
+        {
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO `etudiant` (nom,prenom,cne) values ('" + nom.ToString() + "','" + prenom.ToString() + "','" + cne.ToString() + "')", connexion);
+            try
+            {
+                connexion.Open();
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("Bien ajoutées");
+                    actualiser();
+
+                }
+                connexion.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                connexion.Close();
+            }
+        }
+
+        public void supprimer()
+        {
+            int l = dataGridView1.CurrentCell.RowIndex;
+            int ID = (int)dataGridView1.Rows[l].Cells[0].Value;
+            MySqlCommand cmd = new MySqlCommand("DELETE FROM etudiant WHERE `etudiant`.`id`=" + ID + "", connexion);
+            try
+            {
+                connexion.Open();
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("L'étudiant N° " + ID.ToString() + " a été bien supprimer");
+                    actualiser();
+                }
+                connexion.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                connexion.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vid();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nom = textBox1.Text;
+            string prenom = textBox2.Text;
+            string cne = textBox3.Text;
+            ajout(nom, prenom, cne);
+            vid();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            supprimer();
+        }
+
+        int indice = -1;
+        int ID2;
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (indice == -1)
+            {
+                indice = dataGridView1.CurrentCell.RowIndex;
+                ID2 = (int)dataGridView1.Rows[indice].Cells[0].Value;
+                textBox1.Text = dataGridView1.Rows[indice].Cells[1].Value.ToString();
+                textBox2.Text = dataGridView1.Rows[indice].Cells[2].Value.ToString();
+                textBox3.Text = dataGridView1.Rows[indice].Cells[3].Value.ToString();
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button5.Enabled = false;
+                button6.Text = "Valider";
+            }
+            else
+            {
+                MySqlCommand cmd = new MySqlCommand("update etudiant set nom='" + textBox1.Text + "', prenom='" + textBox2.Text + "', cne='" + textBox3.Text + "' where id=" + ID2 + "", connexion);
+                try
+                {
+                    connexion.Open();
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        MessageBox.Show("L'etudiant N " + ID2.ToString() + " a ete bien modifier");
+                        actualiser();
+                    }
+                    connexion.Clone();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    connexion.Close();
+                }
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button5.Enabled = true;
+                button6.Text = "Modifier";
+                actualiser();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ projetC#BD/Form2.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace projetC_BD
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.CrystalReport11 = new projetC_BD.CrystalReport1();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(12, 12);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.CrystalReport11;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1203, 459);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Retour Home";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 538);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private CrystalReport1 CrystalReport11;
+        private System.Windows.Forms.Button button1;
+    }
+}
